aab2a56299d03c90a0cca1d4c1227e155b32c752
tangenhao2006 <tangenhao2006@gmail.com>
2026-08-03 21:24:07 -0400

    Update LC167_Two_Sum_II.py

--- 02_Two_Pointers/LC167_Two_Sum_II.py
+++ 02_Two_Pointers/LC167_Two_Sum_II.py
@@ -17,7 +17,8 @@ class Solution(object):
                 end -= 1
 # 利用数组升序，首尾双指针求和；和偏小左指针右移、偏大右指针左移，命中目标下标 + 1 返回。
 # 思路
-# Use two pointers on sorted array from two ends. Move left right if sum < target, move right left if sum > target, return indices + 1 when matched.
+# Use two pointers on sorted array from two ends. Move left right if sum < target, move right left if sum > target,
+# return indices + 1 when matched.
 # Complexity
 # Time: O(n)
 # Space: O(1)
